eab1977e3859dba3711de43e7b8ca50b34f6a4fe
PiyushBarde <piyushbarde1@gmail.com>
2021-12-10 18:57:49 +0530

    ADDED:daily wage uc2

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,11 +1,17 @@
 public class EmpWageComputation {
 
-   public static void main(String[] args){
-      int IS_FULL_TIME = 1;
-      double empCheck = Math.floor(Math.random() * 10) % 2;
-      if (empCheck == IS_FULL_TIME)
-         System.out.println("Employee is Present");
-      else
-         System.out.println("Employee is Absent");
-      }
+      final static int IS_FULL_TIME = 1;
+      final static int EMP_RATE_PER_HOUR = 20;
+		public static void main(String[] args){
+      int empHrs = 0;
+		int empWage = 0;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+			if ( empCheck == IS_FULL_TIME)
+				empHrs = 8;
+			else{
+				System.out.println("Employee is Absent");
+				}
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			System.out.println("Emp Wage:" + empWage);
+	}
 }
