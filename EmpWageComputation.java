fdcb1d55399a24b4527a072635085246283e6c8e
PiyushBarde <piyushbarde1@gmail.com>
2021-12-19 21:08:35 +0530

    UPDATED: save total wage by company

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,9 +2,21 @@ public class EmpWageComputation {
 	 public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
 
-	public static int computeEmpWage(String company, int empRatePerHour,
-									 int numOfWorkingDays, int maxHoursPerMonth){
+	private final  String company;
+	private final  int empPerHour;
+	private final  int numOfWorkingDays;
+	private final  int maxHoursPerMonth;
+	private int totalEmpWage;
 
+
+	public static int computeEmpWage(String company, int empRatePerHour,
+									 int numOfWorkingDays, int maxHoursPerMonth) {
+			this.company = company;
+			this.empPerHour = empPerHour;
+			this.numOfWorkingDays =numOfWorkingDays;
+			this.maxHoursPerMonth=maxHoursPerMonth;
+	}
+	public void computeEmpWage(){
 			int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 			while ( totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS ) {
 			totalWorkingDays++;
@@ -24,13 +36,15 @@ public class EmpWageComputation {
 			System.out.println("Days#: " + totalWorkingDays + " Emp Hr: " +empHrs);
 		}
 		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Total Emp Wage: " + totalEmpWage);
-		return totalEmpWage;
 	}
-
+	public string toString(){
+		return "Total Emp Wage for Company : " + company + "is" + totalEmpWage;
+	}
 	public static void main(String[] args) {
-		computeEmpWage("DMart",20,2,10);
-		computeEmpWage("Relience",10,4,20);
-
+		EmpWageBuilderObject dmart = new EmpWageBuilderObject("DMart", 20 , 2, 10);
+		EmpWageBuilderObject reliance = new EmpWageBuilderObject("reliance", 10 , 4, 20);
+		dmart.computeEmpWage();
+		System.out.println(dmart);
+		reliance.computeEmpWage();
+		System.out.println(reliance);
 	}
-}
